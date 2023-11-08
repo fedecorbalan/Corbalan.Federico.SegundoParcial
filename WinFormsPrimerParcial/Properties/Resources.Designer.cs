309e7fe365a0516f25637348ccbcf871ace0be84
fede <fedecorbalan10@gmail.com>
2023-11-08 18:35:44 -0300

    cambios

--- WinFormsPrimerParcial/Properties/Resources.Designer.cs
+++ WinFormsPrimerParcial/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WinFormsPrimerParcial.Properties {
+namespace WinFormsSegundoParcial.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WinFormsPrimerParcial.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WinFormsPrimerParcial.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WinFormsSegundoParcial.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
