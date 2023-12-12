eb63569bd636e4601d76d204f20f9ad221c5293d
fede <fedecorbalan10@gmail.com>
2023-12-11 22:56:54 -0300

    documentacion hornero

--- PrimerParcial/Hornero.cs
+++ PrimerParcial/Hornero.cs
@@ -7,22 +7,42 @@ using System.Threading.Tasks;
 
 namespace PrimerParcial
 {
+    /// <summary>
+    /// Representa un Hornero, que es una clase derivada de Animal.
+    /// </summary>
     public class Hornero : Animal
     {
+        /// <summary>
+        /// Se declaran variables que indican si el Hornero tiene alas y su velocidad en Km/H.
+        /// </summary>
         public bool tieneAlas;
         public int velocidadKmH;
 
+        /// <summary>
+        /// Constructor predeterminado de la clase Hornero.
+        /// </summary>
         public Hornero() 
         {
             this.tieneAlas = true;
             this.velocidadKmH = 30;
         }
+        /// <summary>
+        /// Constructor parametrizado de la clase Hornero.
+        /// </summary>
+        /// <param name="velocidadKmH">La velocidad de vuelo en kilómetros por hora.</param>
+        /// <param name="tieneAlas">Indica si el Hornero tiene alas.</param>
+        /// <param name="nombre">El nombre del Hornero.</param>
+        /// <param name="esPeludo">Indica si el Hornero es peludo.</param>
+        /// <param name="especie">La especie del Hornero.</param>
         public Hornero(int velocidadKmH, bool tieneAlas, string nombre, bool esPeludo, Eespecies especie) : base(nombre, esPeludo, especie)
         {
             this.tieneAlas = tieneAlas;
             this.velocidadKmH = velocidadKmH;
         }
-
+        /// <summary>
+        /// Devuelve una representación en cadena del Hornero.
+        /// </summary>
+        /// <returns>Una cadena que representa el Hornero.</returns>
         public override string Mostrar()
         {
             StringBuilder sb = new StringBuilder();
@@ -35,25 +55,36 @@ namespace PrimerParcial
             return sb.ToString();
 
         }
+
+        /// <summary>
+        /// Devuelve una representación en cadena del Hornero utilizando el método Mostrar.
+        /// </summary>
+        /// <returns>Una cadena que representa el Hornero.</returns>
         public override string ToString()
         {
             return this.Mostrar();
         }
+        /// <summary>
+        /// Implementación del método para emitir el sonido del Hornero.
+        /// </summary>
+        /// <returns>El sonido emitido por el Hornero.</returns>
         public override string EmitirSonido()
         {
             string sonidoHornero = "Pi-pi-pi!";
             return sonidoHornero;
         }
-
+        /// <summary>
+        /// Devuelve la cantidad de extremidades del Hornero, que es 2.
+        /// </summary>
+        /// <returns>La cantidad de extremidades del Hornero.</returns>
         public override int CantidadExtremidades()
         {
             return 2;
         }
-        public int CantidadExtremidades(int cantidadExtremidades)
-        {
-            return cantidadExtremidades;
-        }
-
+        /// <summary>
+        /// Actualiza las propiedades del Hornero con las propiedades del Hornero proporcionado.
+        /// </summary>
+        /// <param name="h">El Hornero con las propiedades actualizadas.</param>
         public void ActualizarHornero(Hornero h)
         {
             this.nombre = h.nombre;
