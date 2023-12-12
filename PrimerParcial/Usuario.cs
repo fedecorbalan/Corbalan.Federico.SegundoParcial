44c45b528b6467481f23b12571ee442639fc84d8
fede <fedecorbalan10@gmail.com>
2023-12-12 00:42:10 -0300

    documentacion clase usuario

--- PrimerParcial/Usuario.cs
+++ PrimerParcial/Usuario.cs
@@ -6,19 +6,34 @@ using System.Threading.Tasks;
 
 namespace WinFormsPrimerParcial
 {
+    /// <summary>
+    /// Representa a un usuario del sistema.
+    /// </summary>
     public class Usuario
-    { 
+    {
+        /// <summary>
+        /// Prooiedades que obtienen o establecen el nombre, apellio, correo,
+        /// perfil, legajo y/o clave ddel usuario.
+        /// </summary>
         public string nombre { get; set; }
-
         public string apellido { get; set; }
-
         public string correo { get; set; }
         public string perfil { get; set; }
-
         public int legajo { get; set; }
         public string clave { get; set; }
-
+        /// <summary>
+        /// Constructor predeterminado de la clase Usuario.
+        /// </summary>
         public Usuario() { }
+        /// <summary>
+        /// Constructor parametrizado de la clase Usuario.
+        /// </summary>
+        /// <param name="nombre">Nombre del usuario.</param>
+        /// <param name="apellido">Apellido del usuario.</param>
+        /// <param name="correo">Correo electrónico del usuario.</param>
+        /// <param name="perfil">Perfil del usuario.</param>
+        /// <param name="legajo">Número de legajo del usuario.</param>
+        /// <param name="clave">Clave del usuario.</param>
         public Usuario(string nombre, string apellido, string correo, string perfil, int legajo, string clave) 
         { 
             this.perfil = perfil;
@@ -28,7 +43,5 @@ namespace WinFormsPrimerParcial
             this.clave = clave;
             this.legajo = legajo;
         }
-        
-
     }
 }
