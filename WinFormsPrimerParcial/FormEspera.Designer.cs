cad3b92283990479eb5a735346175b8fd78ba56e
fede <fedecorbalan10@gmail.com>
2023-12-12 00:14:41 -0300

    creacion form espera

---
+++ WinFormsPrimerParcial/FormEspera.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace WinFormsSegundoParcial
+{
+    partial class FormEspera
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // FormEspera
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightPink;
+            ClientSize = new Size(545, 281);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FormEspera";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Aguarde un momento...";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ WinFormsPrimerParcial/FormEspera.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsSegundoParcial
+{
+    public partial class FormEspera : Form
+    {
+        public FormEspera()
+        {
+            InitializeComponent();
+        }
+        public void Mostrar()
+        {
+
+        }
+    }
+}
