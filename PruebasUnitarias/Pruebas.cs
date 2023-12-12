e416246e08f0b842279011f0638637bac350a372
fede <fedecorbalan10@gmail.com>
2023-12-12 02:37:18 -0300

    doc pruebas

--- PruebasUnitarias/Pruebas.cs
+++ PruebasUnitarias/Pruebas.cs
@@ -5,9 +5,15 @@ using WinFormsPrimerParcial;
 
 namespace PruebasUnitarias
 {
+    /// <summary>
+    /// Clase que contiene pruebas unitarias para la funcionalidad de la aplicación.
+    /// </summary>
     [TestClass]
     public class Pruebas
     {
+        /// <summary>
+        /// Prueba unitaria para la comparación de animales por cantidad de extremidades.
+        /// </summary>
         [TestMethod]
         public static void TestComparacion1()
         {
@@ -23,7 +29,9 @@ namespace PruebasUnitarias
 
             Console.WriteLine(resultado);
         }
-
+        /// <summary>
+        /// Prueba unitaria para la comparación de animales por Especie.
+        /// </summary>
         [TestMethod]
         public static void TestComparacion2()
         {
@@ -39,6 +47,10 @@ namespace PruebasUnitarias
 
             Console.WriteLine(resultado);
         }
+        /// <summary>
+        /// Prueba unitaria para verificar el sonido emitido por un hornero.
+        /// </summary>
+        [TestMethod]
         public static void TestSonido()
         {
             try 
@@ -60,6 +72,9 @@ namespace PruebasUnitarias
               
             }
         }
+        /// <summary>
+        /// Método que realiza la comparación de animales por cantidad de extremidades.
+        /// </summary> 
         public static int OrdenarAnimalesPorCantidadDeExtremidades(Ornitorrinco ornitorrinco, Rana rana)
         {
 
@@ -77,6 +92,9 @@ namespace PruebasUnitarias
             }
 
         }
+        /// <summary>
+        /// Método que realiza la comparación de animales por especie.
+        /// </summary>
         public static int OrdenarAnimalesPorEspecie(Ornitorrinco ornitorrinco, Rana rana)
         {
             if (ornitorrinco.especie == rana.especie)
@@ -92,6 +110,9 @@ namespace PruebasUnitarias
                 return -1;
             }
         }
+        /// <summary>
+        /// Método que simula la emisión de sonido por parte de un hornero.
+        /// </summary>
         public static string EmitirSonido()
         {
             string sonidoHornero = "Pi-pi-pi!";
